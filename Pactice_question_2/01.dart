48a2a9eaf2948543d1058de4ccc12113a69545c8
sunjirsanuj <sunjirislam2019@gmail.com>
2026-06-22 15:29:24 +0600

    delete a folder

--- Pactice_question_2/01.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-    print("Enter a number: ");
-    int a = int.parse(stdin.readLineSync()!);
-    if(a%2 == 0) print("$a is even");
-    else print("$a is odd");
-}
