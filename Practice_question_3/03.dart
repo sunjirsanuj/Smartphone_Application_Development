27832bce80c7efd389ad8e03136a9dcca97a3c8c
sunjirsanuj <sunjirislam2019@gmail.com>
2026-06-26 16:22:21 +0600

    greeting function

---
+++ Practice_question_3/03.dart
@@ -0,0 +1,12 @@
+/* 3. Create a function called greet that takes a name as an argument and prints a greeting message.
+      For example, greet(“John”) should print “Hello, John”. */
+
+void greet(String name){
+    print("Hello, $name.");
+}
+
+void main(){
+    greet("John");
+}
+
+// Output: Hello, John.
