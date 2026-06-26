e8444e025c8bb348088ea4ae90f44cd061c110f5
sunjirsanuj <sunjirislam2019@gmail.com>
2026-06-26 16:45:56 +0600

    sum problem

---
+++ Practice_question_3/08.dart
@@ -0,0 +1,9 @@
+// 8. Write a function in Dart named add that takes two numbers as arguments and returns their sum.
+
+void sum(int n1, int n2) => print("Sum: ${n1+n2}");
+
+void main(){
+    sum(5, 10);
+}
+
+// Output: Sum: 15
