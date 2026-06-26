f9e4e10cd52b790a56d196e54feb3a73e8947f8f
sunjirsanuj <sunjirislam2019@gmail.com>
2026-06-26 16:20:26 +0600

    simple funtion problem

---
+++ Practice_question_3/01.dart
@@ -0,0 +1,11 @@
+// 1. Write a program in Dart to print your own name using function.
+
+void printName(String name){
+    print("My name is $name.");
+}
+
+void main(){
+    printName("Sunjir");
+}
+
+// Output: My name is Sunjir.
