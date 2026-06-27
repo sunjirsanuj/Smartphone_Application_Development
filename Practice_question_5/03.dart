1cf09042ca0d996e72836d63947f0e84ccecb099
sunjirsanuj <sunjirislam2019@gmail.com>
2026-06-27 23:49:30 +0600

    print pwd

---
+++ Practice_question_5/03.dart
@@ -0,0 +1,9 @@
+// 3. Write a dart program to get the current working directory.
+
+import 'dart:io';
+
+void main() {
+  print(Directory.current.path);
+}   
+
+// Output: D:\Varsity\Smartphone_Application_Development
